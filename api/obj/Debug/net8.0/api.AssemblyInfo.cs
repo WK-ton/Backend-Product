f69c152910670e01ef2914723bc039edcc93feac
Worapong <129503107+WK-ton@users.noreply.github.com>
2024-04-02 13:58:15 +0700

    [update]

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb40e432ad223789efcb090034c6d9aded618b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2c1f9414e35c7bc0988960dacbd9a1ff238589")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
